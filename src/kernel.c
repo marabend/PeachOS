7c05a43a67e762f565d3770c201051bb14a81633
Marcel <dev.marcel.abend@gmail.com>
2025-10-10 12:15:48 +0200

    filesystem continued

--- src/kernel.c
+++ src/kernel.c
@@ -99,7 +99,10 @@ void kernel_main()
     // Enable the system interrupts
     enable_interrupts();
 
-    char buf[20];
-    strcpy(buf, "hello");
+    int fd = fopen("0:=/hello.txt", "r");
+    if (fd)
+    {
+        print("We opened hello.txt\n");
+    }
     while(1){}
 }
